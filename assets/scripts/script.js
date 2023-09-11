86a94e201d009c48441fc55a0cdf0064afd92287
Fatima Ferreira <ifatinha99@gmail.com>
2023-09-11 19:55:33 -0300

    finish section testimonial

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,20 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: true
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: true,
-                loop: false
-            }
+$('.owl-carousel').owlCarousel({
+    center: true,
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
         }
-    });
+    }
 });
